2e8b7f9cfbca0323089bcd5b4329ae2ee068ea24
Marco Hinz <mh.codebro@gmail.com>
2016-03-10 11:36:57 +0100

    init

---
+++ gomali.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+)
+
+func main() {
+	foundIssue := false
+
+	if file, err := os.Open("/data/github/vim-galore/README.md"); err != nil {
+		log.Fatal(err)
+	} else {
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			foundIssue = foundIssue || checkRules(scanner.Text())
+		}
+		// if err = scanner.Err(); err != nil {
+		// 	file.Close()
+		// 	log.Fatal(err)
+		// }
+		file.Close()
+	}
+
+	if foundIssue {
+		os.Exit(1)
+	}
+	os.Exit(0)
+}
+
+func checkRules(s string) bool {
+	foundIssue := ruleTooLong(s)
+	return foundIssue
+}
+
+func ruleTooLong(s string) bool {
+	if len(s) > 80 {
+		log.Printf("Too long: %s\n", s)
+		return true
+	} else {
+		log.Printf("Len: %d\n", len(s))
+	}
+	return false
+}
